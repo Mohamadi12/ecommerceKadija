e7ad54a242de56e27940ec1b1d9d5663a907e25e
Nana Mohamadi <adilnana78@gmail.com>
2024-12-11 21:07:43 +0100

    Configuration products and create product and add uplodthng sdk

--- src/components/global/dashboard/DashboardNavigation.tsx
+++ src/components/global/dashboard/DashboardNavigation.tsx
@@ -1,34 +1,47 @@
-import Link from "next/link"
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
   {
-    name: 'Dashboard',
-    href: '/dashboard'
+    name: "Dashboard",
+    href: "/dashboard",
   },
   {
-    name: 'Orders',
-    href: '/dashboard/orders'
+    name: "Orders",
+    href: "/dashboard/orders",
   },
   {
-    name: 'Products',
-    href: '/dashboard/products'
+    name: "Products",
+    href: "/dashboard/products",
   },
   {
-    name: 'Categories',
-    href: '/dashboard/categories'
+    name: "Categories",
+    href: "/dashboard/categories",
   },
-]
+];
 
 const DashboardNavigation = () => {
+  const pathname = usePathname();
   return (
     <>
-      {links.map((link) =>(
-        <Link href={link.href} key={link.href}>
+      {links.map((link) => (
+        <Link
+          href={link.href}
+          key={link.href}
+          className={cn(
+            link.href === pathname
+              ? "text-foreground"
+              : "text-muted-foreground hover:text-foreground"
+          )}
+        >
           {link.name}
         </Link>
       ))}
     </>
-  )
-}
+  );
+};
 
-export default DashboardNavigation
+export default DashboardNavigation;
