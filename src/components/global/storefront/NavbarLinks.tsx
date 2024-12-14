82ebfb67bc22d58a5794b2a3934c4d83693d2e8f
Nana Mohamadi <adilnana78@gmail.com>
2024-12-14 01:04:06 +0100

    Reglage de link

--- src/components/global/storefront/NavbarLinks.tsx
+++ src/components/global/storefront/NavbarLinks.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const navbarLinks = [
   {
@@ -24,10 +28,21 @@ export const navbarLinks = [
 ];
 
 const NavbarLinks = () => {
+  const location = usePathname();
+
   return (
-    <div className="hidden md:flex justify-center items-center gap-x-4 ml-8">
+    <div className="hidden md:flex justify-center items-center gap-x-2 ml-8">
       {navbarLinks.map((item) => (
-        <Link href={item.href} key={item.id} className="font-medium">
+        <Link
+          href={item.href}
+          key={item.id}
+          className={cn(
+            location === item.href
+              ? "bg-muted"
+              : "hover:bg-muted hover:bg-opacity-75",
+            "group p-2 font-medium rounded-md"
+          )}
+        >
           {item.name}
         </Link>
       ))}
