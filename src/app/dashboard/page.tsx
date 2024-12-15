b53e780a535ce90bcc875ea8019672143088dfd4
Nana Mohamadi <adilnana78@gmail.com>
2024-12-15 13:16:55 +0100

    Recente transaction

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,11 +8,13 @@ import {
 } from "@/components/ui/card";
 import React from "react";
 import { DashboardStats } from "./DashboardStats";
+import { RecentSales } from "./RecentSales";
 
 const Dashboard = () => {
   return (
     <>
       <DashboardStats />
+      
       <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3 mt-10">
         <Card className="xl:col-span-2">
           <CardHeader>
@@ -22,53 +24,8 @@ const Dashboard = () => {
             </CardDescription>
           </CardHeader>
         </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Recent sales</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-8">
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden sm:flex h-9 w-9">
-                <AvatarFallback>KT</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium">Khadija Triki </p>
-                <p className="text-sm text-muted-foreground">test@gmail.com</p>
-              </div>
-              <p className="mt-auto font-medium">+1,999.00</p>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden sm:flex h-9 w-9">
-                <AvatarFallback>KT</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium">Khadija Triki </p>
-                <p className="text-sm text-muted-foreground">test@gmail.com</p>
-              </div>
-              <p className="mt-auto font-medium">+1,999.00</p>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden sm:flex h-9 w-9">
-                <AvatarFallback>KT</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium">Khadija Triki </p>
-                <p className="text-sm text-muted-foreground">test@gmail.com</p>
-              </div>
-              <p className="mt-auto font-medium">+1,999.00</p>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden sm:flex h-9 w-9">
-                <AvatarFallback>KT</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium">Khadija Triki </p>
-                <p className="text-sm text-muted-foreground">test@gmail.com</p>
-              </div>
-              <p className="mt-auto font-medium">+1,999.00</p>
-            </div>
-          </CardContent>
-        </Card>
+
+       <RecentSales/>
       </div>
     </>
   );
